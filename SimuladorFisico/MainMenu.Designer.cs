c10c2543de4d35c3f17f5d7b94e4460a14abdfd7
ezrillex <39611157+ezrillex@users.noreply.github.com>
2019-04-24 16:43:30 -0600

    Add files via upload

--- SimuladorFisico/MainMenu.Designer.cs
+++ SimuladorFisico/MainMenu.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // c_BTParab
             // 
-            this.c_BTParab.Location = new System.Drawing.Point(29, 46);
+            this.c_BTParab.Location = new System.Drawing.Point(8, 12);
             this.c_BTParab.Name = "c_BTParab";
             this.c_BTParab.Size = new System.Drawing.Size(137, 23);
             this.c_BTParab.TabIndex = 0;
@@ -45,7 +45,7 @@
             // 
             // c_BTCentrif
             // 
-            this.c_BTCentrif.Location = new System.Drawing.Point(196, 46);
+            this.c_BTCentrif.Location = new System.Drawing.Point(8, 41);
             this.c_BTCentrif.Name = "c_BTCentrif";
             this.c_BTCentrif.Size = new System.Drawing.Size(137, 23);
             this.c_BTCentrif.TabIndex = 1;
@@ -55,11 +55,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(29, 89);
+            this.button1.Location = new System.Drawing.Point(12, 70);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(133, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Pendulo";
+            this.button1.Text = "Pendulo Simple";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -67,7 +67,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(509, 135);
+            this.ClientSize = new System.Drawing.Size(157, 115);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.c_BTCentrif);
             this.Controls.Add(this.c_BTParab);
