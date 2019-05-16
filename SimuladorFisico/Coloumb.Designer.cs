9734507c5b03e0e7cdd061d6dabcc3ea8e8dc88a
ezrillex <39611157+ezrillex@users.noreply.github.com>
2019-05-16 20:11:53 +0200

    50% coloumb

--- SimuladorFisico/Coloumb.Designer.cs
+++ SimuladorFisico/Coloumb.Designer.cs
@@ -32,6 +32,9 @@
             this.trackBar2 = new System.Windows.Forms.TrackBar();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label_distance = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             this.SuspendLayout();
@@ -39,25 +42,27 @@
             // trackBar1
             // 
             this.trackBar1.Location = new System.Drawing.Point(12, 475);
-            this.trackBar1.Maximum = 21;
+            this.trackBar1.Maximum = 20;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(312, 45);
             this.trackBar1.TabIndex = 0;
             this.trackBar1.Value = 5;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.TrackBar1_ValueChanged);
             // 
             // trackBar2
             // 
-            this.trackBar2.Location = new System.Drawing.Point(460, 475);
-            this.trackBar2.Maximum = 21;
+            this.trackBar2.Location = new System.Drawing.Point(660, 475);
+            this.trackBar2.Maximum = 20;
             this.trackBar2.Name = "trackBar2";
             this.trackBar2.Size = new System.Drawing.Size(312, 45);
             this.trackBar2.TabIndex = 1;
             this.trackBar2.Value = 16;
+            this.trackBar2.ValueChanged += new System.EventHandler(this.TrackBar2_ValueChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 523);
+            this.label1.Location = new System.Drawing.Point(12, 523);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 13);
             this.label1.TabIndex = 2;
@@ -66,17 +71,47 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(457, 523);
+            this.label2.Location = new System.Drawing.Point(657, 523);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Carga 2";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 523);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "-5 uC";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(707, 523);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "+5 uC";
+            // 
+            // label_distance
+            // 
+            this.label_distance.AutoSize = true;
+            this.label_distance.Location = new System.Drawing.Point(318, 25);
+            this.label_distance.Name = "label_distance";
+            this.label_distance.Size = new System.Drawing.Size(47, 13);
+            this.label_distance.TabIndex = 6;
+            this.label_distance.Text = "distance";
+            // 
             // Coloumb
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(1000, 561);
+            this.Controls.Add(this.label_distance);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.trackBar2);
@@ -100,5 +135,8 @@
         private System.Windows.Forms.TrackBar trackBar2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label_distance;
     }
 }
--- SimuladorFisico/Coloumb.cs
+++ SimuladorFisico/Coloumb.cs
@@ -15,9 +15,9 @@ namespace SimuladorFisico
         private Timer tick;
         private Rectangle q1;
         private Rectangle q2;
-        private bool q1_hover = false;
-        private bool q2_hover = false;
         private System.Drawing.Graphics MyGraphics;
+        private Pen blackpen = new Pen(Color.Black, 3);
+        private System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
 
         public Coloumb()
         {
@@ -25,45 +25,35 @@ namespace SimuladorFisico
             MyGraphics = this.CreateGraphics();
             InitTick();
 
-            q1 = new Rectangle(ClientRectangle.Width / 4 - 25, ClientRectangle.Height / 2 - 25, 50, 50);
-            q2 = new Rectangle((ClientRectangle.Width / 4) * 3 - 25, ClientRectangle.Height / 2 - 25, 50, 50);
+            q1 = new Rectangle(ClientRectangle.Width / 4, ClientRectangle.Height / 2 - 25, 50, 50);
+            q2 = new Rectangle((ClientRectangle.Width / 4) * 3, ClientRectangle.Height / 2 - 25, 50, 50);
         }
 
         public void InitTick()
         {
             tick = new Timer();
             tick.Tick += new EventHandler(processTick);
-            tick.Interval = 33; // in miliseconds
+            tick.Interval = 100; // in miliseconds
             tick.Start();
         }
 
         public void processTick(object sender, EventArgs e)
         {
-            this.Invalidate();
-            
-            
-                
-            
-        }
+            this.Refresh();
+            MyGraphics.FillEllipse(myBrush, q1);
+            MyGraphics.DrawEllipse(blackpen, q1);
+            MyGraphics.FillEllipse(myBrush, q2);
+            MyGraphics.DrawEllipse(blackpen, q2);
 
-        private void Coloumb_Paint(object sender, PaintEventArgs e)
-        {
-            Pen blackpen = new Pen(Color.Black, 3);
-
-            System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
-            
-            e.Graphics.FillEllipse(myBrush, q1);
-            e.Graphics.DrawEllipse(blackpen, q1);
-
-            e.Graphics.FillEllipse(myBrush, q2);
-            e.Graphics.DrawEllipse(blackpen, q2);
-
-            
+            MyGraphics.DrawLine(blackpen, q1.Location.X +25, q1.Location.Y -100, q1.Location.X + 25 + ConvertValueToUnit(trackBar1.Value) * 15, q1.Location.Y - 100);
+            label_distance.Text = (q2.Location.X - q1.Location.X).ToString();
+            //label_distance.Text = ClientRectangle.Width.ToString();
         }
 
         private void Coloumb_FormClosed(object sender, FormClosedEventArgs e)
         {
             this.Owner.Show();
+            tick.Stop();
         }
 
         private void Coloumb_MouseMove(object sender, MouseEventArgs e)
@@ -74,6 +64,77 @@ namespace SimuladorFisico
         private void Coloumb_MouseClick(object sender, MouseEventArgs e)
         {
             
+        }
+
+        private void TrackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            label3.Text = ConvertValueToUnit(trackBar1.Value) + " uC";
+        }
+
+        private int ConvertValueToUnit(int a) {
+            switch (a)
+            {
+                case 0:
+                    return -10;
+                case 1:
+                    return -9;
+                case 2:
+                    return -8;
+                case 3:
+                    return -7;
+                case 4:
+                    return -6;
+                case 5:
+                    return -5;
+                case 6:
+                    return -4;
+                case 7:
+                    return -3;
+                case 8:
+                    return -2;
+                case 9:
+                    return -1;
+                case 10:
+                    return 0;
+                case 11:
+                    return 1;
+                case 12:
+                    return 2;
+                case 13:
+                    return 3;
+                case 14:
+                    return 4;
+                case 15:
+                    return 5;
+                case 16:
+                    return 6;
+                case 17:
+                    return 7;
+                case 18:
+                    return 8;
+                case 19:
+                    return 9;
+                case 20:
+                    return 10;
+                default:
+                    return 0;
+            }
+        }
+
+        private void TrackBar2_ValueChanged(object sender, EventArgs e)
+        {
+            label4.Text = ConvertValueToUnit(trackBar2.Value) + " uC";
+        }
+
+        private void Coloumb_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private double force(int q1, int q2, int distance)
+        {
+            double k = 9000000000;
+            return 0;
         }
     }
 }
